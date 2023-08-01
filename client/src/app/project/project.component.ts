d3fca8a5eaedf0765ece4964cb928cbe32d3d972
Cesar Fuentes <fuentesivancesar@gmail.com>
2023-08-01 10:49:05 -0500

    Added images to project component

--- client/src/app/project/project.component.ts
+++ client/src/app/project/project.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
+  styleUrls: ['./project.component.scss'],
 })
 export class ProjectComponent {
-
+  @Input() version: string;
+  @Input() type: string;
+  @Input() name: string;
+  @Input() url: string;
+  @Input() comments: string[];
+  @Input() images: string[];
 }
