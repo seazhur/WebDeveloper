088f772960dfad1addfb12abec9b4364f9dd5cb8
Cesar Fuentes <fuentesivancesar@gmail.com>
2023-08-02 15:54:08 -0500

    Added basic animations

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import AOS from 'aos';
+
+AOS.init();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'client';
+  // AOS.init();
 }
---
+++ client/src/app/javascript/animation.js
@@ -0,0 +1,17 @@
+console.log("Is this thing on?");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+// TODO: identify elements in dom with ANGULAR instead
